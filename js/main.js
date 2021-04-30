2620d9aa3f63ac52642f6b75f0b5f22380747eab
entsarawad <71596512+entsarawad@users.noreply.github.com>
2021-04-30 20:46:27 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var tasks =[]
+if (localStorage.getItem('tasks')==null){
+    tasks=[]
+}else{
+    tasks =JSON.parse(localStorage.getItem('tasks'))
+    display()
+    console.log(tasks);
+}
+function getdata() {
+    var taskName =document.getElementById('taskName').value
+    var task = {
+        name:taskName,
+    }
+    tasks.push(task)
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+    display()
+    clearInputs() 
+}
+function display(){
+    var copaya =""
+    for (var i=0; i<tasks.length; i++) {
+        copaya += `<div class="col-md-12 text-center mt-5 card p-2">
+        ${tasks[i].name}
+        <i class="fas fa-times"></i>
+    </div>`
+    }
+    document.getElementById('demo').innerHTML=copaya
+    }
+ function clearInputs() {
+     taskName.value =""
+ }
+function Delete(){
+      products.splice(index,1)
+      local storage.setItem('products',JSON.stringify(products))
+      display()
+}
